48d82553ae2e46fa00432b0a07286876888b84e7
Jorge Ferreiro <me@jgferreiro.com>
2015-10-07 10:33:28 +0200

    Minor changes on cesar encription

--- assigment1/cesarEncription_main.py
+++ assigment1/cesarEncription_main.py
@@ -12,12 +12,14 @@ def checkNumber(userInput):
 	return userInput.isdigit()
 
 def readMessage():
-	valid = False; 
 	message = '';
+	valid = False; 
 	while(not valid):
 		# Iterate until input message has not have any numeric element (number)
-		message = raw_input('Introduce your message: ');
+		message = raw_input('> Introduce your message: ');
 		valid = checkInput(message);
+		if (not valid):
+			print '\t Come on! Numbers are not permited in this program :P'; 
 
 	return message;
 
@@ -25,13 +27,13 @@ def readMessage():
 # iterates until inputed number is correct.
 
 def readNumber():
-	valid = False;
 	number = -1;
+	valid = False;
 	while(not valid):
-		number = raw_input('Introduce your shift Number: ');
+		number = raw_input('> Introduce your shift Number: ');
 		valid = checkNumber(number);
 		if (not valid):
-			print 'Come on! this is not a number :P'; 
+			print '\t Come on! this is not a number :P'; 
 
 	return number;
 
@@ -42,48 +44,61 @@ def userInput():
 	number = readNumber(); 
 
 	# Compose and object and returns to the user
-	userInput = {'message': message, 'number': number};
+	userInput = {
+		'message': message, 
+		'number': number
+	};
 	return userInput;
 
+# Character to Number: Converts a given integer to 
+# corresponding character on ASCII code.
+def characterToNumber(char):
+	number = -1;
+  
+	if (char == 'z' or char == 'Z'):
+		if (char == 'z'):
+			number = ord('a');
+		elif (char == 'Z'):
+			number = ord('A');
+		number -= 1;
+	else:
+		number = ord(char); # Character to Number
+
+	return number;
+
 def simpleEncription(message, shiftNumber):
-	
+
 	if (type(shiftNumber) is not int):
 		shiftNumber = int(shiftNumber); # Int casting of shiftNumber when typed is not expressed as integer. Sometimes may ocurr the program interprets a string...
 	
 	auxChar = '';
 	encriptedMsg = list();
 	shiftNumber = shiftNumber % 26; # English alfhabet has 26 elements. when user shifs more than 26, then make the modulus! if not, an error will crash lines below.
+	tmpNum = 0;
 
 	# Convert each character of the array into a
 	# number, then sum the shif and finally convert this
 	# number into the alfabhet letter corresponding.
 	 
 	for char in message:
+		tmpNum = characterToNumber(char);
+		tmpNum += shiftNumber; # Add shift number to the character number on Ascii
 		
-		if (char == 'z'):
-			auxChar = ord('a') - 1;
-		elif (char == 'Z'):
-			auxChar = ord('A') - 1;
-		else:
-			auxChar = ord(char);
-
-		auxChar += shiftNumber;
-		encriptedChar = chr(auxChar);
-		encriptedMsg.append(encriptedChar);
+		encriptedChar = chr(tmpNum); # Convert from number to char.
+		encriptedMsg.append(encriptedChar); # Add encripted message to the list
 
 	return encriptedMsg;
 
+# Main program
 
 userInput = userInput(); # Returns a valid message and shif number from user in a list.
 message = userInput['message']; # Local variable for the message inputed by the user
 number = userInput['number']; # Local variable for the number inputed by the user
 
 encriptedMsgList = simpleEncription(message, number);
-
-print encriptedMsgList
-
 outputMsg = '';
+
 for index, char in enumerate(encriptedMsgList):
 	outputMsg += char;
 
-print outputMsg
+print outputMsg	
