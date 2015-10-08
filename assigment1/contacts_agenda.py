e924b03cb6fec825d78f23ba25facedf7c5d7d92
Jorge Ferreiro <me@jgferreiro.com>
2015-10-08 16:16:26 +0200

    Load from JSON done

--- assigment1/contacts_agenda.py
+++ assigment1/contacts_agenda.py
@@ -1,19 +1,37 @@
 import json
 
-filename = "agenda.json";
-contacts = []; #dictionary for saving all contacts
+filename = "test1.json"
+agenda = None #dictionary for saving all agenda (contacts, whatever)
 
 # Load users from JSON into a dictionary
 def loadUsers():
-	global contacts
+	global filename
+	global agenda
 
 	try:
-		fileData = open(filename, 'r')
-		tmpContacts = json.load(fileData)
-		contacts = tmpContacts['contacts'] # Save contacts in global variable
+		data = open(filename, 'r'); # try to open JSON file
+		jsonData = json.load(data) # parse json object
+		agenda = jsonData # save agenda object into global variable "agenda"
 	except:
-		print "File not found"
-		contacts = [{}] # set empty dictionary
+		print "File not found! No problem dude. We're creating and empty list"
+		agenda = '{"contancts" : []}' # set empty dictionary
 
-loadUsers();
-print contacts[1]
+def saveUsers():
+	global contacts
+
+	json_array = ''; # empty json object
+
+	if (contacts == None):
+		json_array = '{"agenda" : []}' # empty agenda object
+	else:
+		# format contacts dictionaty into json string
+		json_array += json.dumps(contacts);
+		print json_array
+
+	fileManager = open('test2.json', 'a');
+	fileManager.write(json_array);
+
+
+loadUsers()
+# saveUsers()
+#print contacts[1]['name']
