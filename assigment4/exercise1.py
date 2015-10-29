6fa23dccb87068286d0ce41f2a735429aea27066
Jorge Ferreiro <me@jgferreiro.com>
2015-10-29 17:57:05 +0100

    Exercise 1

--- assigment4/exercise1.py
+++ assigment4/exercise1.py
@@ -5,23 +5,21 @@ cursor = conn.cursor()
 
 def createUniversityTable(cursor):
 	cursor.execute("DROP TABLE IF EXISTS University")
-	cursor.execute("CREATE TABLE University(universityName TEXT, community TEXT, max_capacity INTEGER, PRIMARY KEY(universityName))")
+	cursor.execute("CREATE TABLE University(Nombre_Univ TEXT, Comunidad TEXT, Plazas INTEGER, PRIMARY KEY(Nombre_Univ))")
 
 def createStudentTable(cursor):
 	cursor.execute("DROP TABLE IF EXISTS Students")
-	cursor.execute("CREATE TABLE Students(studentID TEXT, name TEXT, grade REAL, correction_value INT, PRIMARY KEY(studentID))")
+	cursor.execute("CREATE TABLE Students(ID TEXT, Nombre_Est TEXT, Nota REAL, Valor INT, PRIMARY KEY(ID))")
 	
 def createApplicationsTable(cursor):
 	cursor.execute("DROP TABLE IF EXISTS Aplications");
-	cursor.execute("CREATE TABLE Aplications(studentID TEXT, universityName TEXT, degree TEXT, decition TEXT, PRIMARY KEY(universityName, studentID, degree), FOREIGN KEY(universityName) REFERENCES University(niversityName), FOREIGN KEY(studentID) REFERENCES Students(studentID))");
+	cursor.execute("CREATE TABLE Aplications(ID TEXT, Nombre_Univ TEXT, Carrera TEXT, Decision TEXT, PRIMARY KEY(ID, Nombre_Univ, Carrera), FOREIGN KEY(Nombre_Univ) REFERENCES University(Nombre_Univ), FOREIGN KEY(ID) REFERENCES Students(ID))");
 
 def createDatabase(cursor):
 	createUniversityTable(cursor)
 	createStudentTable(cursor)
 	createApplicationsTable(cursor)
-	return 0
 
 createDatabase(cursor)
 cursor.close()
 conn.commit()
-
