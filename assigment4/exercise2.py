e5af76f4454198ed44c09e59b1b50390b1519cdb
Jorge Ferreiro <me@jgferreiro.com>
2015-10-29 17:57:45 +0100

    Inserting data into database DONE

---
+++ assigment4/exercise2.py
@@ -0,0 +1,40 @@
+import sqlite3
+
+dbfile = "managmentSystem.sqlite3"
+
+# t = table's name | f = filename
+t_university    = "University"
+f_university    = "./dbFiles/universities.txt"
+t_students      = "Students"
+f_students      = "./dbFiles/students.txt"
+t_aplications   = "Aplications"
+f_aplications   = "./dbFiles/solicitudes.txt"
+
+# DB configuration
+def config(dbfile):
+	conn   = sqlite3.connect(dbfile)
+	cursor = conn.cursor()
+	return conn, cursor
+
+config = config(dbfile);
+conn   = config[0]
+cursor = config[1]
+
+# Inserting data into the database
+
+def insertFromFile(filename, table, cursor, conn):
+	fileManger = open(filename)
+	rows = [line.rstrip().split(',') for line in fileManger]
+	rows = [str(tuple(rec)) for rec in rows]
+
+	for row in rows:
+		cursor.execute('insert into ' + table + ' values ' + row)
+
+	if (conn): conn.commit()
+
+insertFromFile(f_university, t_university, cursor, conn)
+insertFromFile(f_students, t_students, cursor, conn)
+insertFromFile(f_aplications, t_aplications, cursor, conn)
+
+cursor.close()
+
