769a8c6ff0ae924559bb82c81bb9f442ae907ce9
Jorge Ferreiro <me@jgferreiro.com>
2015-10-29 18:34:37 +0100

    First query done

---
+++ assigment4/exercise3.py
@@ -0,0 +1,35 @@
+import sqlite3
+
+dbfile = "managmentSystem.sqlite3"
+
+# t = table's name | f = filename
+t_university    = "University"
+f_university    = "./dbFiles/universities.txt"
+t_students      = "Students"
+f_students      = "./dbFiles/students.txt"
+t_aplications   = "Aplications"
+f_aplications   = "./dbFiles/solicitudes.txt"
+
+# DB configuration
+def config(dbfile):
+	conn   = sqlite3.connect(dbfile)
+	cursor = conn.cursor()
+	return conn, cursor
+
+config = config(dbfile);
+conn   = config[0]
+cursor = config[1]
+
+def firstQuery(cursor):
+	#cursor.execute('Select * From Students JOIN Aplications ON Students.ID=Aplications.ID WHERE Students.Valor<1000 AND Aplications.Nombre_Univ="Universidad Complutense de Madrid" AND Aplications.Carrera="Informatica" AND Aplications.Decision="Si"')
+	cursor.execute('Select Nombre_Est, Nota, Decision FROM Students JOIN Aplications ON Students.ID=Aplications.ID WHERE Students.Valor<1000 AND Aplications.Nombre_Univ="Universidad Complutense de Madrid" AND Aplications.Carrera="Informatica"')
+
+	for line in cursor:
+		print line[0] + ", " + str(line[1]) + ", " + line[2]
+
+def thirdQuery(cursor):
+	cursor.execute('');
+
+firstQuery(cursor);
+thirdQuery(cursor);
+
