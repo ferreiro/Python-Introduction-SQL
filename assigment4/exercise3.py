60e9e1f0e3da172ff79ad3bf87d68ed7cda438f3
Jorge Ferreiro <me@jgferreiro.com>
2015-10-30 11:39:00 +0100

    Fourth query done [OK]

--- assigment4/exercise3.py
+++ assigment4/exercise3.py
@@ -24,6 +24,7 @@ def firstQuery(cursor):
 	#cursor.execute('Select * From Students JOIN Aplications ON Students.ID=Aplications.ID WHERE Students.Valor<1000 AND Aplications.Nombre_Univ="Universidad Complutense de Madrid" AND Aplications.Carrera="Informatica" AND Aplications.Decision="Si"')
 	cursor.execute('Select Nombre_Est, Nota, Decision FROM Students JOIN Aplications ON Students.ID=Aplications.ID WHERE Students.Valor<1000 AND Aplications.Nombre_Univ="Universidad Complutense de Madrid" AND Aplications.Carrera="Informatica"')
 
+	print "First query result:"
 	for line in cursor:
 		print line[0] + ", " + str(line[1]) + ", " + line[2]
 
@@ -33,29 +34,47 @@ def thirdQuery(cursor):
 	sqlquery = """SELECT * FROM Students WHERE Students.ID not IN \
 	              (Select Students.ID FROM Students JOIN Aplications WHERE Students.ID=Aplications.ID)"""
 
-	cursor.execute(sqlquery);
+	cursor.execute(sqlquery); # Execute the query in order to get in cursor all the students that id doesn't appear on the aplications table
 	
 	for line in cursor:
 		lineList = []
 		lineList.append(line[0]); #ID
-		lineList.append("Universidad de Arizona"); #Name
+		lineList.append("Universidad de Jaen"); #Name
 		lineList.append("Informatica"); #Carrera
 		lineList.append("Si"); #Insert also the decition for the aplications 
 		#lineList.append(line[2]); #nota media
 		noApliedStudents.append(tuple(lineList));
 
 	for entry in noApliedStudents:
-		#print str(entry)
 		cursor.execute("INSERT INTO Aplications values (?, ?,?,?) ", (entry[0], entry[1], entry[2], entry[3]) );
-		#insertQuery = "INSERT INTO Aplications values " + str(tuple(entry))
-		#cursor.execute(insertQuery);
 	
-	print "thirdQuery complete successfully"
+	print "Third query complete successfully...[OK]"
 	conn.commit()
 
-	#JOIN Aplications ON Students.ID=Aplications.ID
+def fourthQuery(cursor):
+	aplications = []
+
+	#Devolver todos los estudiantes que querian estudiar economicas pero que no fueron admitidos en la unviersidad
+	cursor.execute("SELECT * FROM Aplications WHERE Aplications.Decision='No' AND Aplications.Carrera = 'Economia' ");
+
+	for entry in cursor:
+		newAplication = []
+		newAplication.append(entry[0]); #ID
+		newAplication.append("Universidad de Jaen"); #Name
+		newAplication.append("Economia"); #Carrera
+		newAplication.append("Si"); #Insert also the decition for the aplications
+		aplications.append(newAplication);
+	
+	for row in aplications:
+		cursor.execute("INSERT INTO Aplications values (?, ?,?,?) ", (row[0], row[1], row[2], row[3]) );
+
+	#Insertar nuevas entradas admitiendoles en la universidad de jaen
+	print "TODO"
+
 #firstQuery(cursor);
-thirdQuery(cursor);
+#firstQuery(cursor);
+#thirdQuery(cursor);
+fourthQuery(cursor);
 
 conn.commit()
 cursor.close()
