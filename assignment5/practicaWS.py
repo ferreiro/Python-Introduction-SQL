868a0511c979409f86110557f976ffa3c8c09ca7
luisfelipekai <luisfelipekai@gmail.com>
2015-11-09 00:03:37 +0100

    All image urls saved, but there's an error when creating dir

--- assignment5/practicaWS.py
+++ assignment5/practicaWS.py
@@ -8,6 +8,7 @@ Created on Thu Nov  5 14:54:59 2015
 import urllib.request
 from bs4 import BeautifulSoup
 import re
+import os
 
 url = 'http://trenesytiempos.blogspot.com.es/'
 html = urllib.request.urlopen(url).read()
@@ -17,15 +18,25 @@ sopa = BeautifulSoup(html, "html.parser")
 enlaces =sopa.find_all(href=re.compile("/2015"),class_="post-count-link")
 
 #filling the array with its values
+dirList = []
 enlaceList = []
-for enlace in enlaces:
-    enlaceList.append(enlace['href'])
 
+for enlace in enlaces:
+    path = (enlace.text).strip()
+    enlaceList.append(enlace['href'])
+    #dirList.append(os.path.join(path)) 
+    # Python breaks '/' character, so it's creating a lot of directories
+
+#Creating so many Directories... ERROR     
+#for directory in dirList:
+#    os.makedirs(directory)
+    
 imgListTemp = []
-           
-"""for enlace in enlaceList:
-    html = urllib.request.urlopen(enlace).read()
-    sopa = BeautifulSoup(html, "html.parser")
-    img = sopa.find_all('img', {"border":"0"})
-    for imgUrl in img:
-        imgListTemp.append(imgUrl['src'])"""
+
+#filling imgList with all the images of each link in enlaceList           
+for enlace in enlaceList:
+    img = urllib.request.urlopen(enlace).read()
+    sopa = BeautifulSoup(img, "html.parser")
+    all_images = sopa.find_all("img", border="0")
+    for imgs in all_images:
+        imgListTemp.append(imgs['src'])
