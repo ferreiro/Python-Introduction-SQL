057af8e414dd1a3b34b648453654da007cbcab64
tommaso innocenti <innotommy@gmail.com>
2015-11-12 15:35:33 +0100

    user table created

--- assignment6/dataBase.py
+++ assignment6/dataBase.py
@@ -30,8 +30,14 @@ def createTableList(cursor):
 	SQLtables.append(notesTable);
 	
 	UserTable = """
-		create table Tags(
-			
+		create table User(
+			UserID integer PRIMARY KEY AUTOINCREMENT,
+			Email text,
+			Password text,
+			Name text,
+			Surname text,
+			Dateofbirthday text,
+			City text,
 		)
 	"""
 	SQLtables.append(UserTable);
